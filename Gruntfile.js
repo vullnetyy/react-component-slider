b23a112fb0027fae0b19f8b2ed9e077a62535e0b
Vullnet Dyla <vullnet.dyla@gmail.com>
2016-08-31 00:58:03 +0200

    Minified so that it can be browserified again

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,11 +16,19 @@ module.exports = function(grunt) {
                 files: ["src/**/*.js", "example.js"],
                 tasks: ["browserify"]
             }
+        },
+        uglify: {
+            static_mappings: {
+                files: [
+                    {src: 'index.js', dest: 'index.js'}
+                ]
+            }
         }
     });
 
     grunt.loadNpmTasks("grunt-browserify");
     grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask("default", ["browserify"]);
 };
